7023934bf1b77d0adc1cbaf53c8eb487ccf1428e
Sumit Badsara <sumit7engineer@gmail.com>
2017-11-29 04:44:24 +0530

    Update and rename style.css to dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+	if(session_status() == PHP_SESSION_NONE )
+		session_start();
+	include 'connection.php';
+	if(isset($_POST["logout"]))
+	{
+		session_unset();
+		header("Location: index.php");
+	}
+?>
+<!Doctype html>
+<head>
+<title>User Dashboard</title>
+</head>
+<body>
+	Welcome to Dashboard! <?php echo " ".$_SESSION["GEmail"]; ?>
+	<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	<input type="submit" name="logout" Value="Log Out" />
+	</form>
+</body>
+</html>
